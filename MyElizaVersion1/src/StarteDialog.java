f47fc80318f18372b45ea7f42972c5f638c91abc
olive <olive@192.168.0.17>
2017-12-03 17:22:49 +0100

    sfdf

--- MyElizaVersion1/src/StarteDialog.java
+++ MyElizaVersion1/src/StarteDialog.java
@@ -2,8 +2,7 @@
 public class StarteDialog {
 
 	public static void main(String[] args) {
-		// TODO Automatisch generierter Methodenstub
-
+		Dialogsteuerung ds = new Dialogsteuerung();
+		ds.starteDialog();
 	}
-
 }
